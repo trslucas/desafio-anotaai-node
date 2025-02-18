fca0f754fc337a9c2d6594e565e9b6659b435699
Lucas <trslucas@outlook.com>
2025-02-17 22:45:56 -0300

    feature: SNS on category create

--- src/infra/repositories/category/category.ts
+++ src/infra/repositories/category/category.ts
@@ -1,68 +1,60 @@
-import { CategoryDTO, ICategoryRepository } from "../../../types/category/category";
+// biome-ignore lint/style/useImportType: <explanation>
+import {
+	CategoryDTO,
+	ICategoryRepository,
+} from "../../../types/category/category";
 import Category from "../../schemas/category";
 
-
 export class CategoryRepository implements ICategoryRepository {
-  async insert(category: CategoryDTO): Promise<void> {
-    console.log(category)
-    await Category.create(category); 
-  }
+	async insert(category: CategoryDTO): Promise<void> {
+		console.log(category);
+		await Category.create(category);
+	}
 
-  async findById(id: string): Promise<CategoryDTO | null> {
+	async findById(id: string): Promise<CategoryDTO | null> {
+		const category = await Category.findById(id);
 
-    const category = await Category.findById(id)
+		if (!category) {
+			return null;
+		}
 
+		const categoryDTO = {
+			id: String(category._id),
+			title: category.title,
+			description: category.description,
+			ownerId: String(category.ownerId),
+		};
 
-    if(!category) {
+		return categoryDTO;
+	}
 
-    return null
-    }
+	async findAll(): Promise<CategoryDTO[]> {
+		const categories = await Category.find();
 
+		const mappedCategories: CategoryDTO[] = categories.map((category) => ({
+			id: String(category._id),
+			description: category.description,
+			title: category.title,
+			ownerId: String(category.ownerId),
+		}));
 
-    const categoryDTO = {
-      id: String(category._id),
-      title: category.title,
-      description: category.description,
-      ownerId: String(category.ownerId)
-    }
+		return mappedCategories;
+	}
 
-    return categoryDTO
-  }
+	async editCategory(id: string, newCategoryData: CategoryDTO): Promise<void> {
+		const editedCategory: CategoryDTO = {
+			id: String(newCategoryData?.id),
+			description: newCategoryData?.description,
+			title: newCategoryData?.title,
+			ownerId: newCategoryData.ownerId,
+		};
 
-  async findAll(): Promise<CategoryDTO[]> {
-    const categories = await Category.find()
+		await Category.updateOne({ _id: id }, { $set: editedCategory });
+	}
 
-    const mappedCategories: CategoryDTO[] = categories.map(category => ({
-      id: String(category._id),
-      description: category.description,
-      title: category.title,
-      ownerId: String(category.ownerId)
-    }));
-  
-    return mappedCategories;
-  }
-
-  async editCategory(id: string, newCategoryData: CategoryDTO): Promise<void> {
-    
-  
-
-    const editedCategory: CategoryDTO = {
-      id: String(newCategoryData?.id),
-      description: newCategoryData?.description,
-      title: newCategoryData?.title,
-      ownerId: newCategoryData.ownerId
-
-    }
-
-    await Category.updateOne(
-      { _id: id }, 
-      { $set: editedCategory }
-    );  
-  }
-
-  async deleteCategory(id: string): Promise<void> {
-    await Category.deleteOne({
-      _id: id,
-    })
-  }
+	async deleteCategory(id: string): Promise<void> {
+		await Category.deleteOne({
+			_id: id,
+		});
+	}
 }
